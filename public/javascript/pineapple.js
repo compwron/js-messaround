1ea150e0cbf5c318f88143930e7ac22f1126f5ac
compwron <compiledwrong@gmail.com>
2016-04-09 22:11:46 -0700

    Slightly prettier simple modal

--- public/javascript/pineapple.js
+++ public/javascript/pineapple.js
@@ -1,6 +1,4 @@
 $(function(){
-  // $(".pineapple-modal").hide();
-
   $(".pineapple-launcher, .pineapple-background, .pineapple-close").click(function () {
     $(".pineapple-content, .pineapple-background").toggleClass("active");
   });
