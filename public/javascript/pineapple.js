9d93ec9b4097de088f67f8e7a6eb1c9f61f0e61c
compwron <compiledwrong@gmail.com>
2016-04-09 22:56:39 -0700

    Cleanup of bootstrap modal

--- public/javascript/pineapple.js
+++ public/javascript/pineapple.js
@@ -2,13 +2,4 @@ $(function(){
   $(".pineapple-launcher, .pineapple-background, .pineapple-close").click(function () {
     $(".pineapple-content, .pineapple-background").toggleClass("active");
   });
-
-  $(".get-real-modal-thanks-bootstrap").click(function () {
-    console.log("linda says hi")
-    $('#real-modal-thanks-bootstrap').modal()
-  });
-
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').focus()
-  })
 });
