78c5aa74c2fbc89d0019855d60b51955b97e9a3f
lgoldstein <lgoldstein@groupon.com>
2016-04-12 16:25:00 -0700

    gulppfile from gulp recipes

--- setup.sh
+++ setup.sh
@@ -2,5 +2,10 @@ gem install bundler
 bundle install
 npm install --global gulp-cli
 npm install --save-dev gulp
-# npm install -g browserify
-
+npm install --save-dev browserify
+npm install --save-dev gulp-util
+npm install --save-dev gulp-tap
+npm install --save-dev gulp-tap
+npm install --save-dev gulp-buffer
+npm install --save-dev gulp-sourcemaps
+npm install --save-dev gulp-uglify
