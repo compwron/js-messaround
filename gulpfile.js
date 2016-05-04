4f27d9ad940aed2c8a2080f19e6a5d0182c3eec2
compwron <compiledwrong@gmail.com>
2016-05-04 14:35:51 -0700

    removing dest from linting since it is minified

--- gulpfile.js
+++ gulpfile.js
@@ -11,24 +11,13 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var browserify = require('browserify');
 var transform = require('vinyl-transform');
-eslint = require('gulp-eslint');
+var eslint = require('gulp-eslint');
 
 gulp.task('lint', function () {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-
-    // Remove the exclusions for bootstrap and lib after they're no longer checked in
-    return gulp.src(['**/*.js','!node_modules/**', '!public/bootstrap/**', '!public/javascript/lib/**'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
+    // TODO Remove the exclusions for bootstrap and lib after they're no longer checked in
+    return gulp.src(['**/*.js','!node_modules/**', '!public/bootstrap/**', '!public/javascript/lib/**', '!dest/**'])
         .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
         .pipe(eslint.failAfterError());
 });
 
